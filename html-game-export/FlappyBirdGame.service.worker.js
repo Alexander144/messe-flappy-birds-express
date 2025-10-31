893da4ee1cc1b04ec90793aa7b207e2fc0aebe3b
Alexander Larsen <alexan2405@gmail.com>
2025-10-31 16:39:41 +0100

    no message

--- html-game-export/FlappyBirdGame.service.worker.js
+++ html-game-export/FlappyBirdGame.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1761924946|6231763146';
+const CACHE_VERSION = '1761925140|6426321991';
 /** @type {string} */
 const CACHE_PREFIX = 'FlappyBirdGame-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
