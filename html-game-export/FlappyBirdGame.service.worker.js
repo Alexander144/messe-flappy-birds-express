d8261fcd257f8c8ed16873369d0ae2188391ce19
Alexander Larsen <alexan2405@gmail.com>
2025-11-07 08:35:02 +0100

    Maskot update

--- html-game-export/FlappyBirdGame.service.worker.js
+++ html-game-export/FlappyBirdGame.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1762459222|8150699964';
+const CACHE_VERSION = '1762500848|2204740370';
 /** @type {string} */
 const CACHE_PREFIX = 'FlappyBirdGame-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
