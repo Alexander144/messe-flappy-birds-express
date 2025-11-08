37d99b701dc097824cf0358bba040767a73dfe02
Alexander Larsen <alexan2405@gmail.com>
2025-11-08 19:00:48 +0100

    Firebase

--- html-game-export/FlappyBirdGame.service.worker.js
+++ html-game-export/FlappyBirdGame.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1762500848|2204740370';
+const CACHE_VERSION = '1762624839|7137700695';
 /** @type {string} */
 const CACHE_PREFIX = 'FlappyBirdGame-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
